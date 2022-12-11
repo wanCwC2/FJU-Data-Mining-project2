08b22191ad4223e43388c918d0af271645c84a10
wanCwC2 <WanPcwc0630@gmail.com>
2022-12-11 08:17:35 +0800

    Modify all data

--- ModifyData.py
+++ ModifyData.py
@@ -1,8 +1,29 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import pycountry_convert as pc
 
 df = pd.read_csv('data/project2_train.csv')
 test = pd.read_csv('data/project2_test.csv')
 
-df['family_history'] = df['family_history'].replace({'Yes': 1, 'No': 0})
+#df = df.drop[df[(df['Age']<18) | (df['Age']>80)].index]
+#TypeError: 'method' object is not subscriptable
+#df[(df['Age']<18) | (df['Age']>80)].index: [119, 302, 323, 579, 595, 795, 877, 910]
+df = df.drop([119, 302, 323, 579, 595, 795, 877, 910])
+
+other  = ['A little about you', 'p', 'Nah', 'Enby', 'Trans-female','something kinda male?','queer/she/they','non-binary','All','fluid', 'Genderqueer','Androgyne', 'Agender','Guy (-ish) ^_^', 'male leaning androgynous','Trans woman','Neuter', 'Female (trans)','queer','ostensibly male unsure what that really means','trans']
+male   = ['male', 'Male','M', 'm', 'Male-ish', 'maile','Cis Male','Mal', 'Male (CIS)','Make','Male ', 'Man', 'msle','cis male', 'Cis Man','Malr','Mail']
+female = ['Female', 'female','Cis Female', 'F','f','Femake', 'woman','Female ','cis-female/femme','Female (cis)','femail','Woman','female']
+df['Gender']= df['Gender'].replace(other, 0.5)
+df['Gender']= df['Gender'].replace(male, 1)
+df['Gender']= df['Gender'].replace(female, 0)
+
+for col in df.columns:
+    df[col] = df[col].replace({'Yes': 1, 'No': 0, "Don't know": 0, 'Not sure': 0, 'Maybe':0.5, 'Some of them': 0.5})
+    df[col] = df[col].replace({'Often': 1, 'Sometimes': 0.67, 'Rarely': 0.33, 'Never': 0})
+    df[col] = df[col].replace({'More than 1000': 1, '500-1000': 0.8, '100-500': 0.6, '26-100': 0.4, '6-25': 0.2, '1-5': 0})
+    df[col] = df[col].replace({'Somewhat easy': 0.25, "Don't know": 0.5, 'Somewhat difficult': 0.75, 'Very difficult': 1, 'Very easy': 0})
+    df[col] = df[col].fillna(0)
+
+for col in df.columns:
+  print(col, "is", df[col].unique())
